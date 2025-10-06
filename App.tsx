062e44e613adedd3a71595dd17432f434ed1f283
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-10-06 10:53:48 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import RegisterPage from './components/auth/RegisterPage';
 import DashboardPage from './components/dashboard/DashboardPage';                   
 import { AnimatePresence } from 'framer-motion';                            
 import { Toaster } from 'react-hot-toast';
-import ForgotPasswordPage from './components/auth/ForgotPasswordPage';
+import ForgotPasswordPage from './components/auth/ForgotPasswordPage';                      
 import ResetPasswordPage from './components/auth/ResetPasswordPage';                  
 import ProfilePage from './components/profile/ProfilePage';                     
                   
