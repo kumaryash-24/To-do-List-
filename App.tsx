db03fbab07ea6823d774d1841e7cf10438883086
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2026-06-15 11:56:17 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
       
 import React from 'react';      
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';    
 import { AuthProvider, useAuth } from './context/AuthContext';
 import LoginPage from './components/auth/LoginPage';    
 import RegisterPage from './components/auth/RegisterPage'     ;                                                
